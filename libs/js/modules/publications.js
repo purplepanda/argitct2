33644f006d324004becedec897e3166bfb72360a
AmandaO <aolczak@gmail.com>
2020-03-25 22:55:49 -0400

    update publications

--- libs/js/modules/publications.js
+++ libs/js/modules/publications.js
@@ -1,7 +1,11 @@
 $('.publications__link').on('click', function (e) {
     e.preventDefault();
+    $('.publications__content, .publications__link').removeClass('active');
     $(this).parent().find('.publications__content').toggleClass('active');
     $(this).toggleClass('active');
+    $('#publications').delay(500).animate({
+        scrollTop : 0
+    }, 1000);
 });
 
 $('.publications__close').on('click', function (e) {
